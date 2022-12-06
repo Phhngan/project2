cee32766596aff641cc214ffcaaadeaa2cce42d4
hieutv <namduongbeo49@gmail.com>
2022-12-06 11:20:11 +0700

    6/12 suong suong update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('UserInfor', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('use_lastName');
+            $table->string('use_firstName')->unique();
+            $table->date('use_birth');
+            $table->int('use_gender');
             $table->string('email')->unique();
+            $table->integer('phone');
+            $table->string('pro_name')->nullable();
+            $table->boolean('isAdmin', false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
