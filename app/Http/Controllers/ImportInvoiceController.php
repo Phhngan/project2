b66be7d2060b7d6a7ffc21e09cbe681b5c86bfa6
hieutv <namduongbeo49@gmail.com>
2023-06-28 16:00:04 +0700

    28/06

--- app/Http/Controllers/ImportInvoiceController.php
+++ app/Http/Controllers/ImportInvoiceController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 
 class ImportInvoiceController extends Controller
 {
-    //Hien thi toan bo 
+    //Hien thi toan bo
     function index()
     {
         // $importInvoices = Importinvoice::get();
@@ -36,10 +36,36 @@ class ImportInvoiceController extends Controller
         $unit_id = $request->get('unitId');
         $use_id = $request->get('userId');
         $imp_date = $request->get('importDate');
-        $imp_total = $request->get('importTotal');
+        $prd_id = $request->input('productId');
+        $imp_quantity = $request->get('quantity');
+        $imp_price = $request->get('price');
+        $imp_expiryDate = $request->get('expiryDate');
+        $count = count($prd_id);
+        $total = 0;
+        for ($i = 0; $i < $count; $i++) {
+            $total = $total + ($imp_price[$i] * $imp_quantity[$i]);
+        }
         DB::table('ImportInvoices')->insert(
-            ['unit_id' => $unit_id, 'use_id' => $use_id, 'imp_date' => $imp_date, 'imp_total' => $imp_total]
+            ['unit_id' => $unit_id, 'use_id' => $use_id, 'imp_date' => $imp_date, 'imp_total' => $total]
         );
+        $importInvoices = DB::table('ImportInvoices')
+            ->select('ImportInvoices.*')
+            ->where('ImportInvoices.unit_id', $unit_id)
+            ->where('ImportInvoices.use_id', $use_id)
+            ->where('ImportInvoices.imp_date', $imp_date)
+            ->where('ImportInvoices.imp_total', $total)
+            ->get();
+        foreach ($importInvoices as $importInvoice) {
+            for ($i = 0; $i < $count; $i++) {
+                DB::table('ImportInvoiceDetails')->insert(
+                    [
+                        'imp_id' => $importInvoice->imp_id, 'prd_id' => $prd_id[$i], 'imp_quantity' => $imp_quantity[$i],
+                        'imp_price' => $imp_price[$i], 'imp_expiryDate' => $imp_expiryDate[$i], 'prd_status_id' => 1,
+                        'imp_quantity_left' => $imp_quantity[$i],
+                    ]
+                );
+            }
+        }
         return redirect('admin/importInvoice');
     }
 
@@ -53,11 +79,11 @@ class ImportInvoiceController extends Controller
                 return redirect()->route('error');
             }
             $importInvoices = DB::table('ImportInvoices')
-            ->join('SupplyUnits', 'ImportInvoices.unit_id', '=', 'SupplyUnits.unit_id')
-            ->join('Users', 'ImportInvoices.use_id', '=', 'Users.id')
-            ->select('ImportInvoices.*', 'SupplyUnits.unit_name', 'Users.name', 'Users.use_lastName')
-            ->where('ImportInvoices.imp_id', $importInvoice->imp_id)
-            ->get();
+                ->join('SupplyUnits', 'ImportInvoices.unit_id', '=', 'SupplyUnits.unit_id')
+                ->join('Users', 'ImportInvoices.use_id', '=', 'Users.id')
+                ->select('ImportInvoices.*', 'SupplyUnits.unit_name', 'Users.name', 'Users.use_lastName')
+                ->where('ImportInvoices.imp_id', $importInvoice->imp_id)
+                ->get();
             return view('admin/importInvoice.edit', ['importInvoices' => $importInvoices]);
         } else {
             return view('error/khong-co-quyen-admin');
@@ -73,11 +99,4 @@ class ImportInvoiceController extends Controller
             ->update(['unit_id' => $unit_id, 'use_id' => $use_id, 'imp_date' => $imp_date, 'imp_total' => $imp_total]);
         return redirect('admin/importInvoice');
     }
-
-    // Xoa 1 sp theo id
-    // function delete($imp_id)
-    // {
-    //     DB::table('ImportInvoices')->where('imp_id', $imp_id)->delete();
-    //     return redirect('admin/importInvoice');
-    // }
 }
--- resources/views/admin/importInvoice/create.blade.php
+++ resources/views/admin/importInvoice/create.blade.php
@@ -3,56 +3,51 @@
 @section('title','Thêm hóa đơn nhập hàng')
 
 @section('content')
-    <h1 class="text-center">Tạo hóa đơn nhập hàng mới</h1>
-    <form action="{{url('/admin/importInvoice/create')}}" method="POST">
-        @csrf
-        <br>
-        <label for="unitId">Đơn vị cung cấp:</label>
-        <br>
-        <?php
-             $supplyunits = Illuminate\Support\Facades\DB::table('Supplyunits')
-                ->select('Supplyunits.*')
-                ->get();
-          ?>
-	    <select class="form-control" id="" name="unitId" required>
+<h1 class="text-center">Tạo hóa đơn nhập hàng mới</h1>
+<form action="{{url('/admin/importInvoice/create')}}" method="POST">
+    @csrf
+    <br>
+    <label for="unitId">Đơn vị cung cấp:</label>
+    <br>
+    <?php
+    $supplyunits = Illuminate\Support\Facades\DB::table('Supplyunits')
+        ->select('Supplyunits.*')
+        ->get();
+    ?>
+    <select class="form-control" id="" name="unitId" required>
         <option value="" selected="selected">----Chọn đơn vị cung cấp----</option>
-			@foreach($supplyunits as $supplyunit)
+        @foreach($supplyunits as $supplyunit)
         <option value="{{ $supplyunit->unit_id }}">{{ $supplyunit->unit_name }}</option>
-            @endforeach
-	    </select>
-        <br>
-        <label for="userId">Người nhập:</label>
-        <br>
-        <?php
-             $users = Illuminate\Support\Facades\DB::table('Users')
-                ->select('Users.*')->where('Users.pos_id', '>', 1)
-                ->get();
-          ?>
-	    <select class="form-control" id="" name="userId" required>
+        @endforeach
+    </select>
+    <br>
+    <label for="userId">Người nhập:</label>
+    <br>
+    <?php
+    $users = Illuminate\Support\Facades\DB::table('Users')
+        ->select('Users.*')->where('Users.pos_id', '>', 1)
+        ->get();
+    ?>
+    <select class="form-control" id="" name="userId" required>
         <option value="" selected="selected">----Chọn người nhập----</option>
-			@foreach($users as $user)
+        @foreach($users as $user)
         <option value="{{ $user->id }}">{{ $user->use_lastName }} {{ $user->name }}</option>
-            @endforeach
-	    </select>
-        <br>
-        <label for="importDate">Ngày nhập:</label>
-        <br>
-        <input name="importDate" type="date" class="form-control" placeholder="Ngày nhập">
-        <br>
-        <!-- <label for="importTotal">Tổng giá trị đơn nhập:</label>
-        <br>
-        <input name="importTotal" type="number" class="form-control" placeholder="Tổng tiền nhập"> -->
-
-        <!-- thêm sản phẩm trong hóa đơn nhập -->
-        <h5>Thêm sản phẩm vào hóa đơn:</h5>  
-    <table id = "mytable">  
+        @endforeach
+    </select>
+    <br>
+    <label for="importDate">Ngày nhập:</label>
+    <br>
+    <input name="importDate" type="date" class="form-control" placeholder="Ngày nhập">
+    <br>
+    <h5>Thêm sản phẩm vào hóa đơn:</h5>
+    <table id="mytable">
         <tr>
             <th></th>
             <th>Mã sản phẩm</th>
             <th>Số lượng</th>
             <th>Giá sản phẩm</th>
             <th>Ngày hết hạn</th>
-        </tr>  
+        </tr>
         <tr>
             <td style="text-align:center;"><input type="checkbox"></td>
             <td><input name="productId[]" type="text" class="form-control" placeholder="Mã sản phẩm"></td>
@@ -63,111 +58,108 @@
     </table>
     <br>
     <input type="button" class="btn btn-success" value="Thêm sản phẩm" onclick="row()">
-    <input type="button" class="btn btn-danger" value="Xóa" onclick="del()"> 
+    <input type="button" class="btn btn-danger" value="Xóa" onclick="del()">
     <br><br>
-        <button type="submit" class="btn btn-primary" onclick="processForm()">Thêm mới</button>
-    </form>
+    <button type="submit" class="btn btn-primary" onclick="processForm()">Thêm mới</button>
+</form>
 @endsection
 
 @section('js')
-    @parent
-    <script>
-        ClassicEditor
-            .create( document.querySelector( '#editor' ) )
-            .then( editor => {
-                console.log( editor );
-            } )
-            .catch( error => {
-                console.error( error );
-            } );
-    </script>
-    <script>
-        function row() {
-    var mytable = document.getElementById("mytable");
-    var rows = mytable.rows.length;
-    var r = mytable.insertRow(rows);
-    
-    var c1 = r.insertCell(0);
-    var c2 = r.insertCell(1);
-    var c3 = r.insertCell(2);
-    var c4 = r.insertCell(3);
-    var c5 = r.insertCell(4);
-    
-    var checkbox = document.createElement("input");
-    var maSP = document.createElement("input");
-    var quantity = document.createElement("input");
-    var price = document.createElement("input");
-    var hsd = document.createElement("input");
-    
-    checkbox.type = "checkbox";
-    maSP.type = "text";
-    quantity.type = "number";
-    price.type = "number";
-    hsd.type = "date";
-    
-    r.className = "new-row"; // Add a CSS class to the row
-    c1.style.textAlign = "center";
-    
-    maSP.name = "productId[]";
-    quantity.name = "quantity[]";
-    price.name = "price[]";
-    hsd.name = "expiryDate[]";
-    
-    c1.appendChild(checkbox);
-    c2.appendChild(maSP);
-    c3.appendChild(quantity);
-    c4.appendChild(price);
-    c5.appendChild(hsd);
+@parent
+<script>
+    ClassicEditor
+        .create(document.querySelector('#editor'))
+        .then(editor => {
+            console.log(editor);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+</script>
+<script>
+    function row() {
+        var mytable = document.getElementById("mytable");
+        var rows = mytable.rows.length;
+        var r = mytable.insertRow(rows);
+
+        var c1 = r.insertCell(0);
+        var c2 = r.insertCell(1);
+        var c3 = r.insertCell(2);
+        var c4 = r.insertCell(3);
+        var c5 = r.insertCell(4);
+
+        var checkbox = document.createElement("input");
+        var maSP = document.createElement("input");
+        var quantity = document.createElement("input");
+        var price = document.createElement("input");
+        var hsd = document.createElement("input");
+
+        checkbox.type = "checkbox";
+        maSP.type = "text";
+        quantity.type = "number";
+        price.type = "number";
+        hsd.type = "date";
+
+        r.className = "new-row"; // Add a CSS class to the row
+        c1.style.textAlign = "center";
+
+        maSP.name = "productId[]";
+        quantity.name = "quantity[]";
+        price.name = "price[]";
+        hsd.name = "expiryDate[]";
+
+        c1.appendChild(checkbox);
+        c2.appendChild(maSP);
+        c3.appendChild(quantity);
+        c4.appendChild(price);
+        c5.appendChild(hsd);
     }
 
-function del() {
-    var mytable = document.getElementById("mytable");
-    var rows = mytable.rows.length;
-    
-    for (var i = rows - 1; i > 0; i--) {
-        if (mytable.rows[i].cells[0].children[0].checked) {
-        mytable.deleteRow(i);
+    function del() {
+        var mytable = document.getElementById("mytable");
+        var rows = mytable.rows.length;
+
+        for (var i = rows - 1; i > 0; i--) {
+            if (mytable.rows[i].cells[0].children[0].checked) {
+                mytable.deleteRow(i);
+            }
         }
     }
-}
 
-function processForm() {
-  var formData = [];
-    var mytable = document.getElementById("mytable");
-    var rows = mytable.rows.length;
-    
-    for (var i = 1; i < rows; i++) {
-        var row = mytable.rows[i];
-        var productId = row.cells[1].getElementsByTagName("input")[0].value;
-        var quantity = row.cells[2].getElementsByTagName("input")[0].value;
-        var price = row.cells[3].getElementsByTagName("input")[0].value;
-        var expiryDate = row.cells[4].getElementsByTagName("input")[0].value;
-        
-        var data = {
-        productId: productId,
-        quantity: quantity,
-        price: price,
-        expiryDate: expiryDate
-        };
-        
-        formData.push(data);
+    function processForm() {
+        var formData = [];
+        var mytable = document.getElementById("mytable");
+        var rows = mytable.rows.length;
+
+        for (var i = 1; i < rows; i++) {
+            var row = mytable.rows[i];
+            var productId = row.cells[1].getElementsByTagName("input")[0].value;
+            var quantity = row.cells[2].getElementsByTagName("input")[0].value;
+            var price = row.cells[3].getElementsByTagName("input")[0].value;
+            var expiryDate = row.cells[4].getElementsByTagName("input")[0].value;
+
+            var data = {
+                productId: productId,
+                quantity: quantity,
+                price: price,
+                expiryDate: expiryDate
+            };
+
+            formData.push(data);
+        }
+
+        // Process the form data
+        console.log(formData);
     }
-    
-    // Process the form data
-    console.log(formData);
-}
 
-    function del()  
-    {  
-        var mytable = document.getElementById("mytable");  
-        var rows = mytable.rows.length;  
-        for(var i = rows - 1; i > 0; i--)  
-        {  
-            if(mytable.rows[i].cells[0].children[0].checked)  
-            {  
-                mytable.deleteRow(i);  
-            }  
-        }  
-    }  
-    </script>  
+    function del() {
+        var mytable = document.getElementById("mytable");
+        var rows = mytable.rows.length;
+        for (var i = rows - 1; i > 0; i--) {
+            if (mytable.rows[i].cells[0].children[0].checked) {
+                mytable.deleteRow(i);
+            }
+        }
+    }
+</script>
 @endsection
