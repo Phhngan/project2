6fa996a19fd2db1c1905e9e9b4fdfc59233f1050
Phhngan <phamhngan2102@gmail.com>
2023-06-28 10:23:52 +0700

    import đơn hàng

--- resources/views/admin/importInvoice/create.blade.php
+++ resources/views/admin/importInvoice/create.blade.php
@@ -39,10 +39,35 @@
         <br>
         <input name="importDate" type="date" class="form-control" placeholder="Ngày nhập">
         <br>
-        <label for="importTotal">Tổng giá trị đơn nhập:</label>
+        <!-- <label for="importTotal">Tổng giá trị đơn nhập:</label>
         <br>
-        <input name="importTotal" type="number" class="form-control" placeholder="Tổng tiền nhập">
-        <br>
+        <input name="importTotal" type="number" class="form-control" placeholder="Tổng tiền nhập"> -->
+
+        <!-- thêm sản phẩm trong hóa đơn nhập -->
+        <h5>Thêm sản phẩm vào hóa đơn:</h5>  
+    <table id = "mytable">  
+        <tr>
+            <th></th>
+            <th>Mã sản phẩm</th>
+            <th>Số lượng</th>
+            <th>Giá sản phẩm</th>
+            <th>Ngày hết hạn</th>
+        </tr>  
+        <tr>
+            <td style="text-align:center;"><input type=checkbox></td>
+            <td><input name="productId" type="text" class="form-control" placeholder="Mã sản phẩm"></td>
+            <!-- <td><select class="form-control" id="" name="productId" required>
+                <option value="" selected="selected">----Chọn mã sản phẩm----</option>
+            </select></td> -->
+            <td><input name="quantity" type="number" class="form-control" placeholder="Số lượng"></td>
+            <td><input name="price" type="number" class="form-control" placeholder="Giá sản phẩm"></td>
+            <td><input name="expiryDate" type="date" class="form-control" placeholder="Ngày hết hạn"></td>
+        </tr>  
+    </table>  
+    <br>  
+    <input type = button class = "btn btn-success" value = "Thêm sản phẩm" onclick = row()>  
+    <input type = button class = "btn btn-danger" value = "Xóa" onclick = del()>  
+    <br><br>
         <button type="submit" class="btn btn-primary">Thêm mới</button>
     </form>
 @endsection
@@ -59,4 +84,54 @@
                 console.error( error );
             } );
     </script>
+    <script>
+        function row() {
+    var mytable = document.getElementById("mytable");
+    var rows = mytable.rows.length;
+    var r = mytable.insertRow(rows);
+
+    r.classList.add("new-row");
+
+    var c1 = r.insertCell(0);
+    var c2 = r.insertCell(1);
+    var c3 = r.insertCell(2);
+    var c4 = r.insertCell(3);
+    var c5 = r.insertCell(4);
+
+    var checkbox = document.createElement("input");
+    var maSP = document.createElement("input");
+    var quantity = document.createElement("input");
+    var price = document.createElement("input");
+    var hsd = document.createElement("input");
+
+    checkbox.type = "checkbox";
+    maSP.type = "text";
+    maSP.placeholder = "Mã sản phẩm";
+    quantity.type = "number";
+    quantity.placeholder = "Số lượng";
+    price.type = "number";
+    price.placeholder = "Giá sản phẩm";
+    hsd.type = "date";
+    hsd.placeholder = "Ngày hết hạn";
+
+    c1.appendChild(checkbox);
+    c2.appendChild(maSP);
+    c3.appendChild(quantity);
+    c4.appendChild(price);
+    c5.appendChild(hsd);
+    }
+
+    function del()  
+    {  
+        var mytable = document.getElementById("mytable");  
+        var rows = mytable.rows.length;  
+        for(var i = rows - 1; i > 0; i--)  
+        {  
+            if(mytable.rows[i].cells[0].children[0].checked)  
+            {  
+                mytable.deleteRow(i);  
+            }  
+        }  
+    }  
+    </script>  
 @endsection
